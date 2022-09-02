a1844c53edccc1e5dfa3e85d4d8c5e8e56797657
SnowieMischief <97817883+SnowieMischief@users.noreply.github.com>
2022-09-02 16:48:28 -0300

    binary to decimal support added

--- personal/conversorNumerico/src/Conversor/Program.cs
+++ personal/conversorNumerico/src/Conversor/Program.cs
@@ -1,2 +1,4 @@
 ﻿using Library;
 Console.WriteLine(Convertir.dec2bin(10));
+
+Console.WriteLine(Convertir.bin2dec("101010"));
--- personal/conversorNumerico/src/Library/Convertir.cs
+++ personal/conversorNumerico/src/Library/Convertir.cs
@@ -1,4 +1,5 @@
 ﻿namespace Library;
+using System.Linq;
 public static class Convertir
 {
     public static string dec2bin(int entrada){
@@ -11,5 +12,20 @@ public static class Convertir
         Resto.Reverse();
         string outputs = String.Join("",Resto); 
         return outputs;
+    }
+    public static int bin2dec(string entrada){
+        string revEntrada = ""; // reversed entrada
+        foreach(char bit in entrada){ // TODO optimize
+            revEntrada = bit + revEntrada;}
+        double i = 0;
+        List<double> operations = new List<double>{};
+        foreach(char bit in revEntrada){
+            // int num = bit - '0';
+            double num = char.GetNumericValue(bit);
+            operations.Add(num*(Math.Pow(2,i)));
+            i += 1; 
+        }
+        double output = operations.Sum();
+        return Int32.Parse(output.ToString());
     }
 }
